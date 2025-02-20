9fd367ecff40d9c724ec5aadf45d20f4c3bd2639
davidragus <davidruizfp23@ibf.cat>
2025-02-20 17:39:43 +0100

    feat: Add user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,31 +10,48 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
-    {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
-    }
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function definition()
+	{
+		return [
+			'username' => fake()->unique()->userName(),
+			'name' => fake()->firstName(),
+			'surname1' => fake()->lastName(),
+			'surname2' => fake()->lastName(),
+			'email' => fake()->unique()->safeEmail(),
+			'dni' => fake()->randomNumber(9, true),
+			'gender' => fake()->randomElement(['M', 'F', 'N']),
+			'phone_number' => fake()->phoneNumber(),
+			'birthdate' => fake()->dateTimeBetween('-50 years', '-18 years')->format('Y-m-d'),
+			'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+			'remember_token' => Str::random(10),
+		];
+	}
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
+	/**
+	 * Indicate that the model's email address should be unverified.
+	 *
+	 * @return static
+	 */
+	public function unverified()
+	{
+		return $this->state(fn(array $attributes) => [
+			'email_verified_at' => null,
+		]);
+	}
+
+	public function configure()
+	{
+		return $this->afterCreating(function (\App\Models\User $user) {
+			$user->pendingValidations()->create([
+				'status' => 'PENDING',
+				'image_url' => 'pepetron',
+			]);
+			$user->assignRole(fake()->randomElement([1, 2]));
+		});
+	}
 }
