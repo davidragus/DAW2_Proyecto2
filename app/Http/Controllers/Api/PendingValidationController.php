6a595b35927dbbb367b69232a1587c086e16c7b4
davidragus <davidruizfp23@ibf.cat>
2025-02-25 16:21:57 +0100

    feat: Add getValidations API request
    feat: Modify permission seeder
    
    delete: Delete image_url column from pending_validations

--- app/Http/Controllers/Api/PendingValidationController.php
+++ app/Http/Controllers/Api/PendingValidationController.php
@@ -4,63 +4,76 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\PendingValidation;
+use App\Http\Resources\PendingValidationResource;
 use Illuminate\Http\Request;
 
 class PendingValidationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+	/**
+	 * Display a listing of the resource.
+	 */
+	public function index()
+	{
+		$orderColumn = request('order_column', 'created_at');
+		if (!in_array($orderColumn, ['id', 'created_at'])) {
+			$orderColumn = 'created_at';
+		}
+		$orderDirection = request('order_direction', 'desc');
+		if (!in_array($orderDirection, ['asc', 'desc'])) {
+			$orderDirection = 'desc';
+		}
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+		$validations = PendingValidation::orderBy($orderColumn, $orderDirection)
+			->paginate(50);
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+		return PendingValidationResource::collection($validations);
+	}
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(PendingValidation $pendingValidation)
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 */
+	public function create()
+	{
+		//
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(PendingValidation $pendingValidation)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 */
+	public function store(Request $request)
+	{
+		//
+	}
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, PendingValidation $pendingValidation)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 */
+	public function show(PendingValidation $pendingValidation)
+	{
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(PendingValidation $pendingValidation)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 */
+	public function edit(PendingValidation $pendingValidation)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 */
+	public function update(Request $request, PendingValidation $pendingValidation)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 */
+	public function destroy(PendingValidation $pendingValidation)
+	{
+		//
+	}
 }
