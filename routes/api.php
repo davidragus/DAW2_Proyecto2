b7ca6659d624f3d96a230a1397e3beff61093dd8
davidragus <davidruizfp23@ibf.cat>
2025-03-26 16:33:54 +0100

    fix: Fix update user api route

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
 	// Route::get('users', [UserController::class, 'index']);
 	// Route::get('users/{user}', [UserController::class, 'show']);
 	// Route::post('users', [UserController::class, 'store']);
-	// Route::post('users/{user}', [UserController::class, 'update']);
+	Route::post('users/{user}', [UserController::class, 'update']);
 	// Route::delete('users/{user}', [UserController::class, 'destroy']);
 
 	Route::post('users/updateimg', [UserController::class, 'updateimg']); //Listar
