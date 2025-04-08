31d7480dcd5760e173ce8eb7e26229e141d75075
davidragus <davidruizfp23@ibf.cat>
2025-04-08 16:17:35 +0200

    refactor: Modify migrations to adapt tables to bingo games

--- database/migrations/2025_02_06_155151_create_game_rooms_table.php
+++ database/migrations/2025_02_06_155151_create_game_rooms_table.php
@@ -4,31 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('game_rooms', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('game_id');
-            $table->unsignedBigInteger('category_id');
-            $table->string('max_users');
-            $table->integer('connected_users');
-            $table->timestamps();
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('game_rooms', function (Blueprint $table) {
+			$table->id();
+			$table->unsignedBigInteger('game_id');
+			$table->string('max_users');
+			$table->timestamps();
 
-            $table->foreign('game_id')->references('id')->on('games')->onDelete('cascade');
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
-        });
-    }
+			$table->foreign('game_id')->references('id')->on('games')->onDelete('cascade');
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('game_rooms');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('game_rooms');
+	}
 };
