f2621dfecacf782526c4438791d913d1a4701414
Alex Cobas <alexcobasfp@ibf.cat>
2025-04-21 23:17:48 +0200

    modified:   database/seeders/GamesSeeder.php 	new file:   public/images/BannerHome.png 	modified:   resources/js/views/home/index.vue

--- database/seeders/GamesSeeder.php
+++ database/seeders/GamesSeeder.php
@@ -13,7 +13,9 @@ class GamesSeeder extends Seeder
 	 */
 	public function run(): void
 	{	
-		
+		// Creamos bingo, blackjack y ruleta
+
+		// Creamos el juego de bingo
 		$newGame = Game::create([
 			'name' => 'Bingo',
 			'route_path' => 'bingo',
@@ -26,6 +28,24 @@ class GamesSeeder extends Seeder
 			throw new Exception("File not found: $filePath");
 		}
 		$fileIconPath = public_path('images/bingoIcon.svg');
+		if (file_exists($fileIconPath)) {
+			$newGame->addMedia($fileIconPath)->preservingOriginal()->toMediaCollection('games_icons');
+		} else {
+			throw new Exception("File not found: $fileIconPath");
+		}
+		// Creamos el juego de blackjack
+		$newGame = Game::create([
+			'name' => 'Blackjack',
+			'route_path' => 'blackjack',
+			'active' => false,
+		]);
+		$filePath = public_path('images/blackjackGame.webp');
+		if (file_exists($filePath)) {
+			$newGame->addMedia($filePath)->preservingOriginal()->toMediaCollection('games');
+		} else {
+			throw new Exception("File not found: $filePath");
+		}
+		$fileIconPath = public_path('images/iBlackjack.svg');
 		if (file_exists($fileIconPath)) {
 			$newGame->addMedia($fileIconPath)->preservingOriginal()->toMediaCollection('games_icons');
 		} else {
