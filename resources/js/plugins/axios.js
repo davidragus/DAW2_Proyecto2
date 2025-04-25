dc60c71a44085a9a6f328dadd482e84b8203524a
davidragus <davidruizfp23@ibf.cat>
2025-04-25 17:58:34 +0200

    fix: Remove auto redirect to /login

--- resources/js/plugins/axios.js
+++ resources/js/plugins/axios.js
@@ -15,16 +15,16 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.axios.defaults.withCredentials = true
 window.axios.interceptors.response.use(
-    response => response,
-    error => {
-        if (error.response?.status === 401 || error.response?.status === 403 || error.response?.status === 419) {
-            if (location.pathname !== '/login'){
-                location.assign('/login')
-            }
-        }
+	response => response,
+	error => {
+		if (error.response?.status === 401 || error.response?.status === 403 || error.response?.status === 419) {
+			if (location.pathname !== '/login') {
+				// location.assign('/login')
+			}
+		}
 
-        return Promise.reject(error)
-    }
+		return Promise.reject(error)
+	}
 )
 
 /**
